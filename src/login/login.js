61e9b2c7c6f5aae6c351feca52fb3fcbf3bb74b6
Lazaro delas <lazarorytson1@gmail.com>
2023-03-30 19:44:20 -0300

    test

---
+++ src/login/login.js
@@ -0,0 +1,12 @@
+import { View,Text} from "react-native";
+
+
+export default function Login(){
+    return(
+        <View>
+            <View>
+                <Text>Welcome</Text>
+            </View>
+        </View>
+    );
+}
