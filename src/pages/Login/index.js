874f0e2fb187bc515c57c093ea0d4fc76fc70179
John Moreira <johnmoreira42@gmail.com>
2023-04-12 12:00:14 -0300

    Foi criado as rotas iniciais

---
+++ src/pages/Login/index.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Login() {
+  return (
+    <View style={styles.container}>
+      <Text>responsavel Lazaro!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
