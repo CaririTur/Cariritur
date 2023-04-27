1a433ff9eaab57b2d8e46922faba71334d8d8317
Bruno Macêdo <73453125+brunomalenco@users.noreply.github.com>
2023-04-27 17:04:55 -0300

    finalizacao tela home

--- src/components/home page/home.js
+++ src/components/home page/home.js
@@ -1,35 +1,51 @@
-import { Text, View, TouchableOpacity, StyleSheet, Image, FlatList } from "react-native";
+import { Text, View, TouchableOpacity, StyleSheet, Image, FlatList, ScrollView } from "react-native";
 import iconeeee from '../../img/material-symbols_menu.png';
 import dados from '../../mock/eventos'
+import pontos from '../../mock/pontosTuristicos'
 
 export default function Home() {
     return (
-        <View style={styles.container}>
-
+        <>
             <View style={styles.boxSuperior}>
                 <Text style={styles.textoBoxSuperior}>Eventos</Text>
                 <Image source={iconeeee} style={styles.iconMenu} />
             </View>
 
-            <View style={styles.boxEventos}>
 
-                <FlatList
-                    horizontal={true}
-                    showsHorizontalScrollIndicator={false}
-                    keyExtractor={dados => dados.evento}
-                    data={dados}
-                    renderItem={({ item }) => (
-                        <View style={styles.cardEvento}>
-                            <Text style={styles.cardTextoEvento}>{item.evento}</Text>
-                            <Text style={styles.cardDataEvento}>{item.data}</Text>
-                        </View>
-                    )
+            <FlatList
+                data={pontos}
+                showsVerticalScrollIndicator={false}
+                keyExtractor={dados => dados.nome}
+                renderItem={({ item }) => (
+                    <View>
+                        <TouchableOpacity style={styles.cardsPontosTuristicos}>
+                            <Text style={styles.nomePontosTuristicos}>{item.nome}</Text>
+                            <Image source={item.foto} />
+                            <Text style={styles.descricaoPontosTuristicos}>{item.descricao}</Text>
+                        </TouchableOpacity>
+                    </View>
+                )}
+                ListHeaderComponent={
+                    <View style={styles.boxEventos}>
+                        <FlatList
+                            horizontal={true}
+                            showsHorizontalScrollIndicator={false}
+                            keyExtractor={dados => dados.evento}
+                            data={dados}
+                            renderItem={({ item }) => (
+                                <TouchableOpacity style={styles.cardEvento}>
+                                    <Text style={styles.cardTextoEvento}>{item.evento}</Text>
+                                    <Text style={styles.cardDataEvento}>{item.data}</Text>
+                                </TouchableOpacity>
+                            )
+                            }
+                        />
 
-                    }
-                />
-            </View>
-
-        </View>
+                        <Text style={styles.textoPontosTuristicos}>Pontos Turísticos</Text>
+                    </View>
+                }
+            />
+        </>
     )
 }
 
@@ -41,43 +57,72 @@ const styles = StyleSheet.create({
         display: 'flex',
         flexDirection: 'row',
         marginTop: '13%',
+        height: 40,
+        alignItems: 'center'
     },
     textoBoxSuperior: {
         fontSize: 25,
-        textAlign: 'left',
-        alignItems: 'flex-start',
-        justifyContent: 'flex-start',
         marginLeft: 20,
-        marginRight: '60%'
+        marginRight: '60%',
+        fontWeight: '600'
     },
     iconMenu: {
         width: 30,
         height: 30,
         marginTop: 'auto',
+        marginBottom: 'auto',
+        alignItems: 'center',
     },
     boxEventos: {
         height: 150,
-        backgroundColor: 'gray',
         marginTop: 20,
+        marginBottom: 20,
     },
-    cardEvento:{
-        backgroundColor: 'blue',
+    cardEvento: {
+        backgroundColor: '#CDCDCD',
         margin: 10,
         height: 100,
-        width:  150,
+        width: 150,
         borderRadius: 15,
-        padding: 10
+        padding: 10,
+        //Sombreado dos cards
+        elevation: 15
     },
-    cardTextoEvento:{
-        color:"#FFF",
+    cardTextoEvento: {
+        color: "black",
         fontSize: 20,
+        fontWeight: 500,
     },
-    cardDataEvento:{
+    cardDataEvento: {
         marginTop: 'auto',
-        color: '#FFF',
+        color: 'black',
         fontSize: 15,
-        fontStyle:'italic'
-    }
-    
+        fontStyle: 'italic'
+    },
+    textoPontosTuristicos: {
+        marginLeft: 15,
+        fontSize: 25,
+        fontWeight:'500',
+    },
+    cardsPontosTuristicos:{
+        alignItems:'center',
+        paddingLeft: 5,
+        paddingRight: 5,
+        width: '100%',
+        marginBottom: 30
+    },
+    nomePontosTuristicos:{
+        marginRight: 'auto',
+        marginLeft: '5%',
+        marginBottom: 5,
+        fontSize: 18,
+    },
+    descricaoPontosTuristicos:{
+        alignItems:'center',
+        justifyContent: 'center',
+        marginTop: 8,
+        fontSize: 15,
+    },
+
 
 })
---
+++ src/mock/pontosTuristicos.js
@@ -0,0 +1,31 @@
+import museu from '../../src/img/museu.png'
+import peCicero from '../../src/img/pe-cicero.png'
+
+const pontosTuristicos = [
+    {
+        "nome": "Museu Paleontologia",
+        "foto": museu,
+        "descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin elementum suscipit nunc, sed lacinia" 
+    },
+
+    {
+        "nome": "Padre Cícero",
+        "foto": peCicero,
+        "descricao": "Padre Cícero foi um religioso que viveu entre 1844 e 1934, no Ceará, e sua influência atravessou o campo religioso e alcançou a política."
+    },
+
+    {
+        "nome": "Monumento",
+        "foto": museu,
+        "descricao": "Padre Cícero foi um religioso que viveu entre 1844 e 1934, no Ceará, e sua influência atravessou o campo religioso e alcançou a política."
+    },
+
+    {
+        "nome": "Lorem Ipsum",
+        "foto": peCicero,
+        "descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin elementum suscipit nunc"
+    }
+    
+]
+
+export default pontosTuristicos;
