5ff39c79254cd1f9faf2d49401d094fcd277d9fd
Lazaro delas <lazarorytson1@gmail.com>
2023-04-26 21:14:21 -0300

    mudanças

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Cadastro from './src/login/cadastro';
+import Home from './src/home/index';
 
 export default function App() {
   return (
-      <Cadastro/>
+      
   );
 }
 
---
+++ src/home/index.js
@@ -0,0 +1,18 @@
+import { Text, View } from "react-native";
+
+
+export default function Home(){
+    return <>
+    <View>
+        <View>
+            <Text>Eventos</Text>
+        </View>
+
+        <View >
+            <Text>Romaria das Candeias</Text>
+            <Text>12 de Dezembro</Text>
+        </View>
+    </View>
+    </>
+}
+
