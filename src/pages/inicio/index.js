cef896866fbd9a2ec3cb6fedfd3ceb675029d737
jorge <jorge.luiz.g217@gmail.com>
2023-04-27 20:50:06 -0300

    fixing error on merge

--- src/pages/inicio/index.js
+++ src/pages/inicio/index.js
@@ -1,16 +1,27 @@
 import {StatusBar} from 'expo-status-bar';
 import {Image, StyleSheet, Text, TouchableOpacity} from 'react-native';
 import Logo from '../../../assets/logo.png';
+import {useEffect} from "react";
 
 export default function Inicio({navigation}) {
+    useEffect(() => {
+        const time = setTimeout(() => {
+            navigation.navigate("Login");
+
+        }, 2000);
+
+        return () => clearTimeout(time)
+    }, []);
+
     return (
-        <TouchableOpacity style={styles.container} onPress={() => navigation.navigate("Cadastro")}>
+        <TouchableOpacity style={styles.container}>
             <Image source={Logo}/>
             <StatusBar style="auto"/>
             <Text style={{fontWeight: 'bold',}}>Por Grupo Genisys</Text>
         </TouchableOpacity>
-    );
-}
+
+
+)}
 
 const styles = StyleSheet.create({
     container: {
