9496c374920c45a44ce4ae9f450c962c81409651
Bruno Macêdo <73453125+brunomalenco@users.noreply.github.com>
2023-04-27 00:25:21 -0300

    melhorias na tela home

--- src/components/home page/home.js
+++ src/components/home page/home.js
@@ -1,8 +1,9 @@
-import { Text, View, TouchableOpacity, StyleSheet, Image } from "react-native";
-import iconeeee from '../../img/material-symbols_menu.png'
+import { Text, View, TouchableOpacity, StyleSheet, Image, FlatList } from "react-native";
+import iconeeee from '../../img/material-symbols_menu.png';
+import dados from '../../mock/eventos'
 
-export default function Home(){
-    return(
+export default function Home() {
+    return (
         <View style={styles.container}>
 
             <View style={styles.boxSuperior}>
@@ -10,8 +11,22 @@ export default function Home(){
                 <Image source={iconeeee} style={styles.iconMenu} />
             </View>
 
-            <View>
-                
+            <View style={styles.boxEventos}>
+
+                <FlatList
+                    horizontal={true}
+                    showsHorizontalScrollIndicator={false}
+                    keyExtractor={dados => dados.evento}
+                    data={dados}
+                    renderItem={({ item }) => (
+                        <View style={styles.cardEvento}>
+                            <Text style={styles.cardTextoEvento}>{item.evento}</Text>
+                            <Text style={styles.cardDataEvento}>{item.data}</Text>
+                        </View>
+                    )
+
+                    }
+                />
             </View>
 
         </View>
@@ -19,27 +34,50 @@ export default function Home(){
 }
 
 const styles = StyleSheet.create({
-    container:{
+    container: {
         flex: 1,
     },
-    boxSuperior:{
-        display:'flex',
-        flexDirection:'row',
+    boxSuperior: {
+        display: 'flex',
+        flexDirection: 'row',
         marginTop: '13%',
     },
-    textoBoxSuperior:{
+    textoBoxSuperior: {
         fontSize: 25,
-        textAlign:'left',
-        alignItems:'flex-start',
-        justifyContent:'flex-start',
+        textAlign: 'left',
+        alignItems: 'flex-start',
+        justifyContent: 'flex-start',
         marginLeft: 20,
         marginRight: '60%'
     },
-    iconMenu:{
+    iconMenu: {
         width: 30,
         height: 30,
         marginTop: 'auto',
-        
+    },
+    boxEventos: {
+        height: 150,
+        backgroundColor: 'gray',
+        marginTop: 20,
+    },
+    cardEvento:{
+        backgroundColor: 'blue',
+        margin: 10,
+        height: 100,
+        width:  150,
+        borderRadius: 15,
+        padding: 10
+    },
+    cardTextoEvento:{
+        color:"#FFF",
+        fontSize: 20,
+    },
+    cardDataEvento:{
+        marginTop: 'auto',
+        color: '#FFF',
+        fontSize: 15,
+        fontStyle:'italic'
     }
+    
 
 })
