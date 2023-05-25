eece4f515b7da120eaa77670888d2e4f92008ee4
John Moreira <johnmoreira42@gmail.com>
2023-05-25 17:18:26 -0300

    Reformulacao da tabnavigation

--- src/routes/tab_routes.js
+++ src/routes/tab_routes.js
@@ -8,14 +8,19 @@ import {CommonActions, useNavigation} from '@react-navigation/native';
 import New from "../pages/new";
 import Profile from "../pages/profile";
 import Rotas from "../pages/rotas";
-import Eventos from "../pages/eventos";
-import ButtonNew from "./ButtonNew";
+import Eventos from "../pages/Eventos";
 import Home from "../pages/homepage";
 
-import HomeIcon from "../../assets/Home.svg"
-import HomeFillIcon from "../../assets/HomeFill.svg"
-import RoutesIcon from "../../assets/Routes.svg"
-import RoutesFillIcon from "../../assets/RoutesFill.svg"
+import HomeIcon from "../../assets/tabnavigation/Home.svg"
+import HomeFillIcon from "../../assets/tabnavigation/HomeFill.svg"
+import RoutesIcon from "../../assets/tabnavigation/Routes.svg"
+import RoutesFillIcon from "../../assets/tabnavigation/RoutesFill.svg"
+import EventosIcon from "../../assets/tabnavigation/Eventos.svg"
+import EventosFillIcon from "../../assets/tabnavigation/EventosFill.svg"
+import UserIcon from "../../assets/tabnavigation/User.svg"
+import UserFillIcon from "../../assets/tabnavigation/UserFill.svg"
+import AddcircleIcon from "../../assets/tabnavigation/addcircle.svg"
+import CircleFillIcon from "../../assets/tabnavigation/CircleFill.svg"
 
 import CaririTur from "../../assets/components/CaririTur.svg";
 import Menu from "../../assets/components/Menu.svg";
@@ -76,7 +81,7 @@ export default function TabRoutes() {
                 screenOptions={{
                     tabBarShowLabel: false,
                     tabBarStyle: {
-                        borderTopWidth: 0,
+                    borderTopWidth: 0,
                     },
                 }}
             >
@@ -96,33 +101,30 @@ export default function TabRoutes() {
                         )
                     }
                 }}/>
-                <Tab.Screen
-                    name="Novo"
-                    component={New}
-                    options={{
-                        tabBarIcon: ({size, color}) => (
-                            <ButtonNew/>
+             <Tab.Screen name="Novo" component={New} options={{
+                    headerTitle: () => <Header type={'back'} name={"New"}/>,
+                    tabBarIcon: ({focused}) => {
+                        return (
+                            focused ? <CircleFillIcon/> : <AddcircleIcon/>
                         )
-                    }}
-                />
-                <Tab.Screen
-                    name="Eventos"
-                    component={Eventos}
-                    options={{
-                        tabBaricon: ({size, color}) => (
-                            <Feather name="event" size={size} color={color}/>
-                        ),
-                    }}
-                />
-                <Tab.Screen
-                    name="Profile"
-                    component={Profile}
-                    options={{
-                        tabBaricon: ({size, color}) => (
-                            <Feather name="profile" size={size} color={color}/>
-                        ),
-                    }}
-                />
+                    }
+                }}/>
+            <Tab.Screen name="Eventos" component={Eventos} options={{
+                    headerTitle: () => <Header type={'back'} name={"Eventos"}/>,
+                    tabBarIcon: ({focused}) => {
+                        return (
+                            focused ? <EventosFillIcon/> : <EventosIcon/>
+                        )
+                    }
+                }}/>
+           <Tab.Screen name="Profile" component={Profile} options={{
+                    headerTitle: () => <Header type={'back'} name={"Profile"}/>,
+                    tabBarIcon: ({focused}) => {
+                        return (
+                            focused ? <UserFillIcon/> : <UserIcon/>
+                        )
+                    }
+                }}/>
             </Tab.Navigator>
         </>
     );
